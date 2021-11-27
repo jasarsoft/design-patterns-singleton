99044db43167850c5d2722ba63a0971d42efb03a
Edin Jašarević <edinjasar14@gmail.com>
2021-11-27 22:10:03 +0100

    naive signleton

---
+++ DesignSingletonPattern/v1_Naive/Singleton.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignSingletonPattern.v1_Naive
+{
+    public sealed class Singleton
+    {
+        private static Singleton? _instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called.");
+                return _instance ??= new Singleton();
+            }
+        }
+
+        private Singleton()
+        {
+            // cannot be created except within this class
+            Logger.Log("Constructor invoked.");
+        }
+    }
+}
