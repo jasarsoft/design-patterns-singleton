00e9e7c8702e2c033702c60c422acc9371399287
Edin Jašarević <edinjasar14@gmail.com>
2021-11-27 22:19:28 +0100

    static contructors and singletons

---
+++ DesignSingletonPattern/v4_LessLazy/Singleton.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignSingletonPattern.v4_LessLazy
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+
+        // reading this will initialize the _instance
+        public static readonly string GREETING = "Hi!";
+
+        // Tell C# compiler not to mark type as beforefieldinit
+        // (https://csharpindepth.com/articles/BeforeFieldInit)
+        static Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called.");
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            // cannot be created except within this class
+            Logger.Log("Constructor invoked.");
+        }
+    }
+}
---
+++ DesignSingletonPattern/v5_NestedLazy/Singleton.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignSingletonPattern.v5_NestedLazy
+{
+    public sealed class Singleton
+    {
+        // reading this will initialize the instance
+        public static readonly string GREETING = "Hi!";
+        public static Singleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called.");
+                return Nested._instance;
+            }
+        }
+
+        private class Nested
+        {
+            // Tell C# compiler not to mark type as beforefieldinit (https://csharpindepth.com/articles/BeforeFieldInit)
+            static Nested()
+            {
+            }
+            internal static readonly Singleton _instance = new Singleton();
+        }
+
+        private Singleton()
+        {
+            // cannot be created except within this class
+            Logger.Log("Constructor invoked.");
+        }
+    }
+}
